105d44a353ba12906454946692fb32afc94b8cdd
ZodieW <affsrodie@gmail.com>
2024-01-27 22:29:11 -0500

    Adicionando modelo começando desafio portifólio

---
+++ desafios/js/aula04-desafio.js
@@ -0,0 +1,16 @@
+let listaGenerica = [];
+
+let linguagemDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagemDeProgramacao.push('Java');
+linguagemDeProgramacao.push('Ruby');
+linguagemDeProgramacao.push('GoLang');
+
+let tresPorquinhos = ['Prático', 'Heitor', 'Cícero'];
+console.log(tresPorquinhos[0]);
+
+let tresEspiasDemais = ['Sam', 'Alex', 'Clover'];
+console.log(tresEspiasDemais[1]);
+
+let trigemeas = ['Ana', 'Tereza', 'Helena'];
+console.log(trigemeas[2]);
